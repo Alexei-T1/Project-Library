691c63659ac384fa0fc144c776cd6917b7016f78
Alexei-T1 <turencko.alexei@yandex.ru>
2023-04-25 22:51:05 +0400

    add prototype remove button for deleting a book

--- scriptes/Book.js
+++ scriptes/Book.js
@@ -4,12 +4,13 @@ const BOOK = 'book';
 const BOOK_NAME = 'book__name';
 const BOOK_AUTHOR = 'book__author';
 const BOOK_PAGES = 'book__pages';
+const BUTTON_REMOVE = 'button_remove';
 
 export class Book {
   constructor(book) {
     // const {name, author, pages} = book;
     this.bookEl = null;
-
+    this.listBook = {};
     this.creatHTML(book);
     this.addHeandlers();
   }
@@ -31,11 +32,19 @@ export class Book {
       pages.classList.add(BOOK_PAGES);
       pages.innerText = book.pages.value;
 
-      wrapper.append(name, author, pages);
+      const removeButton = document.createElement('button');
+      removeButton.classList.add(BUTTON_REMOVE);
+      removeButton.innerText = 'remove';
+
+      wrapper.append(name, author, pages, removeButton);
 
       this.bookEl = wrapper;
+      Object.assign(this.listBook, { removeButton });
     }
     addHeandlers() {
-
+      this.listBook.removeButton.addEventListener('click',this.remove.bind(this));
+    }
+    remove() {
+      this.bookEl.remove();
     }
 }
